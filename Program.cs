d7cdf45969156cd9e17ced9f1d96ed155269cb72
Nikitos <Doronboom825@gmail.com>
2022-11-17 20:23:11 +0300

    с трудом решил 4 задачу

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
 //a = 2 b = 10 -> max = 10
 //a = -9 b = -3 -> max = -3
 
-Console.WriteLine("Введите первое число");
+/*Console.WriteLine("Введите первое число");
 int number1 = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("Введите второе число");
@@ -18,3 +18,30 @@ else
 {
     Console.WriteLine($"число {number1} меньше {number2}");
 }
+*/
+
+
+
+/*Задача 4
+Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22
+*/
+
+Console.WriteLine("Введите первое число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int number3 = Convert.ToInt32(Console.ReadLine());
+int max;
+
+if (number1>number2)
+max = number1;
+else
+max = number2;
+if (number3>number2)
+max = number3;
+Console.WriteLine($"Максимальное число {max}");
+ 
